15962982a5726a5e919c223a8b85a62fac314b77
ntigi <ntig0001@algonquinlive.com>
2020-06-06 02:34:00 -0400

    finalized the app and made it totally dynamic

--- Cake_form/index.js
+++ Cake_form/index.js
@@ -1,136 +1,132 @@
-//console.log("I am connected");
-
-//we used an associative array!!!!!!!
+/* Set up an associative array
+    The keys represent the size of the cake
+    The values represent the cost of the cake i.e A 10" cake cost's $35 */
 var cake_prices = new Array();
-  cake_prices ["Round6"] = 20;
-  cake_prices ["Round8"] = 25;
-  cake_prices ["Round10"] = 35;
-  cake_prices ["round12"] = 75;
+cake_prices["Round6"] = 20;
+cake_prices["Round8"] = 25;
+cake_prices["Round10"] = 35;
+cake_prices["Round12"] = 75;
+ 
+ /* Set up an associative array 
+    The keys represent the filling type
+    The value represents the cost of the filling i.e. Lemon filling is $5,Dobash filling is $9
+    We use this this array when the user selects a filling from the form */
+ var filling_prices = new Array();
+ filling_prices["None"] = 0;
+ filling_prices["Lemon"] = 5;
+ filling_prices["Custard"] = 5;
+ filling_prices["Fudge"] = 7;
+ filling_prices["Mocha"] = 8;
+ filling_prices["Raspberry"] = 10;
+ filling_prices["Pineapple"] = 5;
+ filling_prices["Dobash"] = 9;
+ filling_prices["Mint"] = 5;
+ filling_prices["Cherry"] = 5;
+ filling_prices["Apricot"] = 8;
+ filling_prices["Buttercream"] = 7;
+ filling_prices["Chocolate Mousse"] = 12;
 
-  //getCakeSizePrice() finds the price based on the size of the cake
-  //Here, we need to take user's selection from radio button selection
 
-  function getCakeSizePrice() {
-    var getCakeSizePrice = 0;
-    //get a reference to the form id="cakeform"
-    var theForm = document.forms["cakeform"];
-    //get a reference to the cake the user chooses by name= selectedCake
-    var selectedCake = theForm.elements["selectedCake"];
-    //here since there are 4 radio buttons selectedCake.length = 4
-    //we loop throughb each radio buttons 
-    for(var i = 0; i < selectedCake.length; i++){
-      //if the radio button is checked
-      if(selectedCake[i].checked){
-        //we set the cakeSizePrice to the value of the selected radio button 
-        //i.e if the user choose the 8" cake we set it to 25
-        //by using the cake_prices array
-        //we get the selected Items value
-        //For example cake_prices ["Round8".value]
-        cakeSizePrice = cake_prices[selectedCake[i].value];
-        //if we get a match then we break out of this loop
-        //no reason to continue if we get a match
-        break;
-      }
+/* find the price based on the size of the cake by taking user's the selection from radio button selection */
+function getCakeSizePrice(){  
+    var cakeSizePrice = 0;
+    var theForm = document.forms["cakeform"]; 
+    var selectedCake = theForm.elements["selectedcake"];
+    for(var i = 0; i < selectedCake.length; i++)
+    {
+        //if the radio button is checked
+        if(selectedCake[i].checked)
+        {
+          cakeSizePrice = cake_prices[selectedCake[i].value];
+          break;
+        }
     }
-    //we return the cakeSizePrice
     return cakeSizePrice;
-    
-  }
+}
 
-  //let's match the values of the drop-down menu to the respective prices
-  //using an associative array. But we are going to use a function which finds the filling price //based on the drop down selection
-  function getFillingPrice(){
+/**
+* This function finds the filling price based on the
+* drop down selection 
+*/
+function getFillingPrice(){
     var cakeFillingPrice = 0;
-    //get a reference to the form id="cakeform"
     var theForm = document.forms["cakeform"];
-    //console.log(theForm);
-    //get a reference to the selected id = "filling"
     var selectedFilling = theForm.elements["filling"];
-    //return selectedFilling;
+    cakeFillingPrice = filling_prices[selectedFilling.value];
+    return cakeFillingPrice;
+}
 
-    //set cakeFilling Price equal to the value user chose
-    //For example filling_price ["Lemon".value] would be equal to 5
-
-    //Let's create the associative array first
-    var filling_price = new Array();
-        filling_prices ["None"] = 0;
-        filling_prices ["Lemon"] = 5;
-        filling_prices ["Custard"] = 5;
-        filling_prices ["Fudge"] = 7;
-        filling_prices ["Mocha"] = 8;
-        filling_prices ["Raspberry"] = 10;
-
-        cakeFillingPrice = filling_prices[selectedFilling.value];
-
-        //finally we return cakeFillingPrice
-        return cakeFillingPrice;
-
-  }
-
-  //console.log(getFillingPrice());
-  //candlesPrices() finds the candles price based on a check box selection
-  function candlesPrice () {
+//find the candles price based on a check box selection
+function calculateCandles(){
     var candlePrice = 0;
-    //Get a reference to the form id="cakeform"
     var theForm = document.forms["cakeform"];
-    //get a reference to the checkbox id="includecandles"
-    var includecandles = theForm.elements["includecandles"];
-    
-    /*Here we simply should have used: 
-    var includecandles = document.getElementById("includecandles"); */
-    
-    //if they checked the box set candlePrice to 5
-    if(includecandles.checked == true){
-      candlePrice = 5;
+    var includeCandles = theForm.elements["includecandles"];
+    if(includeCandles.checked == true)
+    {
+        candlePrice=5;
     }
-    //finally we return the candlePrice
     return candlePrice;
-  }
-  
-  function inscriptionPrice(){
-    var incriptionPrice = 0;
-    var includeinscription = document.getElementById("includeinscription");
-    if(includeinscription.checked){
-      inscriptionPrice = 20;
-    }
-    return inscriptionPrice;
-  }
-  //console.log(inscriptionPrice());
-  
-  /*
-  Here is what we would have done if the user were to enter a quantity 
-  we would have to use the parseInt() method and not an associative array 
+}
 
-  function getQuantity (){
-    //assume form id="theform"
+function calculateInscription(){
+    var inscriptionPrice = 0;
     var theForm = document.forms["cakeform"];
-    //get a reference to the TextBox 
-    var quantity = theForm.elements["quantity"];
-    var howmany = 0;
-    //if the textbox is not blank
-    if(quantity.value!=""){
-      howmany = parseInt(quantity.value);
+    var includeInscription = theForm.elements["includeinscription"];
+    var inscriptions = document.getElementById("inscriptions").value;
+    if(includeInscription.checked == true && inscriptions >= 0){
+        inscriptionPrice = 20 * inscriptions;
+    }else{
+        inscriptionPrice = 0;
     }
-    return howmany;
-  }
-  */
+    console.log(inscriptions);
+    return inscriptionPrice;
+}
+        
+function defineTotal(){
+    var cakePrice = getCakeSizePrice() + getFillingPrice() + calculateCandles() + calculateInscription();
+    return cakePrice;
+}
 
-  function getTotal (){
-    //here we get the total price by calling our function
-    //each function returns a number so by calling them, we add the values they return together
-    var cakePrice = getCakeSixePrice() + 
-                    getFillingPrice() +
-                    candlesPrice() +
-                    inscriptionPrice(); 
+/**
+ * hide the total on homepage
+ */
+function hideTotal(){
+    var result = document.getElementById('totalPrice');
+    result.style.display='none';
+}
 
-    //display the result
-    document.getElementById("totalPrice").innerHTML = "Total Price For Cake $" + cakePrice;
+/**
+ * display the total as the customer clicks 'Check Total'
+ */
+function showTotal(){
+    var result = document.getElementById('totalPrice');
+    result.style.display='block';
+    result.innerHTML = "Your Total Is $" + defineTotal();
+}
 
-    //the program updates the innerHTML of the "totalPrice everytime the totals are calculated"
-  }
-  
-  
-  
-  
-  
-  
+/**
+ * submit user order checking for null total
+ */
+function submitOrder(){
+    var totalPrice = defineTotal();
+    if(totalPrice === 0){
+        location.href = "cakeform.html";
+        return 0;
+    }else{
+        location.href = "contact.html";
+        return totalPrice;
+    }
+}
+
+/**
+ * finalize order, save data and take user to homepage
+ */
+function finalizeOrder(){
+    var customerName = document.getElementById("name").value;
+    var address = document.getElementById("address").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var commment = document.getElementById("message").value;
+    //send user to thanks page
+    location.href = "thanks.html";
+    return customerName + "Ordered a Cake.";
+}
